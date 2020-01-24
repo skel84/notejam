c662b7e4141579a4a8f0560af6e41e124db04799
Francesco Cislaghi <francesco.cislaghi@daimler-x.com>
2020-01-24 15:20:26 +0100

    modify database connection

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -7,8 +7,8 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
-
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql://'+ (os.environ['FLASK_USR']) + ':' + (os.environ['FLASK_PWD']) + '@' + (os.environ['FLASK_DBHOST']) + ':3306/notejam'
 
 class ProductionConfig(Config):
     DEBUG = False
